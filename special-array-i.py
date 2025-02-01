904acb7f975aef7c1127f7489e6fcbae7c4caddb
mohamed meqlad <mohamedmeqlad9@gmail.com>
2025-02-01 23:17:42 +0200

    Create special-array-i.py

---
+++ special-array-i.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def isArraySpecial(self, nums):
+        for i in range(1, len(nums)):
+            if nums[i-1]%2==nums[i]%2:
+                return False
+        
+        return True
